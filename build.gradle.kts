a32d4a2b3d24bac537b2768a529039d0f2cfe0a9
Karl Goffin <karl.goffin@gmail.com>
2020-09-19 18:59:10 +0100

    release 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.kagof"
-version = "1.1.0-SNAPSHOT"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -20,10 +20,35 @@ intellij {
 }
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
     changeNotes("""
-      <ul>
-      <li><b><a href="https://github.com/kagof/intellij-pokemon-progress/releases/tag/1.0.2">1.0.2</a></b> icon compatibility with Retina screens</li>
-      <li><b><a href="https://github.com/kagof/intellij-pokemon-progress/releases/tag/1.0.1">1.0.1</a></b> minor bugfix, backwards compatibility with IntelliJ 2019.3.5</li>
-      <li><b><a href="https://github.com/kagof/intellij-pokemon-progress/releases/tag/1.0.0">1.0.0</a></b> Initial release</li>
-      </ul>
+    <ul>
+        <li><b><a href="https://github.com/kagof/intellij-pokemon-progress/releases/tag/1.1.0">1.1.0</a></b>
+            <ul>
+                <li>New Gen I: Butterfree, Meowth, Slowpoke, Gengar, Koffing, Gyarados, Snorlax, Articuno, Zapdos, Moltres, Mewtwo, Mew</li>
+                <li>New Gen II: Togepi, Wobbuffet</li>
+                <li>New Gen III: Wailmer, Wailord</li>
+                <li>New Gen VIII: Grookey, Rillaboom, Scorbunny, Cinderace, Sobble, Intelleon, Wooloo, Zacian, Zamazenta</li>
+                <li>Lazy loads Pokémon icons</li>
+                <li>Plugin is now dynamically reloadable (uses Listeners instead of Component)</li>
+            </ul>
+        </li>
+        <li><b><a href="https://github.com/kagof/intellij-pokemon-progress/releases/tag/1.0.2">1.0.2</a></b> 
+            <ul>
+                <li>fixes icon compatibility with Retina screens</li>
+            </ul>
+        </li>
+        <li><b><a href="https://github.com/kagof/intellij-pokemon-progress/releases/tag/1.0.1">1.0.1</a></b>
+            <ul>
+                <li>backwards compatibility with IntelliJ 2019.3.5</li>
+                <li>fixes display issue when progress bar goes from indeterminate to determinate</li>
+            </ul>
+        </li>
+        <li><b><a href="https://github.com/kagof/intellij-pokemon-progress/releases/tag/1.0.0">1.0.0</a></b>
+            <ul>
+                <li>Initial release</li>
+                <li>New Gen I: Bulbasaur, Venusaur, Charmander, Charizard, Squirtle, Blastoise, Pikachu, Eevee</li>
+                <li>New Gen VII: Mimikyu</li>
+            </ul>
+        </li>
+    </ul>
       """)
 }
