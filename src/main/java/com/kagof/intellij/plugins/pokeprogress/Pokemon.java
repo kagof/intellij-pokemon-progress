f387cf6ff6ad9cef0c2abedf96161fddea5e4c98
Paola351 <paola.rodilosso@gmail.com>
2020-10-20 01:00:36 +0200

    restored kyogre number

--- src/main/java/com/kagof/intellij/plugins/pokeprogress/Pokemon.java
+++ src/main/java/com/kagof/intellij/plugins/pokeprogress/Pokemon.java
@@ -51,7 +51,7 @@ public enum Pokemon {
     // Gen III
     WAILMER(320, "wailmer", -16, -9, PokemonType.WATER),
     WAILORD(321, "wailord", -35, -35, PokemonType.WATER),
-    KYOGRE(384, "kyogre", -16, -16, PokemonType.WATER),
+    KYOGRE(382, "kyogre", -16, -16, PokemonType.WATER),
     GROUDON(383, "groudon", -20, -12, PokemonType.GROUND),
     RAYQUAZA(384, "rayquaza", -16, -13, PokemonType.DRAGON, PokemonType.FLYING),
     JIRACHI(385, "jirachi", -14, -10, PokemonType.STEEL, PokemonType.PSYCHIC),
